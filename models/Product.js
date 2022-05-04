8368daae8bbf59028783d43b460ef7073a1122df
Brooke Love <savienloveb@gmail.com>
2022-05-03 17:44:13 -0700

    adding in columns to models and routing items into others

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,5 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER, DECIMAL } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
@@ -10,6 +10,36 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+        type: INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    product_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DECIMAL,
+        allowNull: true,
+        validate: {
+            isDecimal: true
+        }
+    },
+    stock: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: 10,
+        validate: {
+            isNumeric: true
+        }
+    },
+    category_id: {
+        type: INTEGER,
+        // this might not be the correct syntax
+        include: [category.id]
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
@@ -7,6 +7,22 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+        type: INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    product_tag: {
+        type: INTEGER,
+        // references back to the product id
+        include: [product.id]
+    },
+    tag_id: {
+        type: INTEGER,
+        // ferences back to the tag id
+        include: [tag.id]
+    }
   },
   {
     sequelize,
