f5397b84657d4cb8392b3a3a58fe710b03f9166f
Brooke Love <savienloveb@gmail.com>
2022-05-03 21:20:54 -0700

    updating CRUD fro tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -34,15 +34,38 @@ router.post('/', (req, res) => {
   Tag.create(req.body)
   .then(newTag => {
       res.json(newTag)
+  }).catch (err => {
+      console.log(err);
+      res.status(500).json({msg: "an error has occured", err});
   })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+      where: {
+          id: req.params.id
+      }
+    }).then (updatedTag => {
+          res.join(updatedTag);  
+    }).catch (err => {
+          console.log(err);
+          res.status(500).json({msg: " an error has occured", err});
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy ({
+      where: {
+          id: req.params.id
+      }
+  }).then (delTag => {
+      res.json(delTag)
+  }).catch (err => {
+      console.log(err);
+      res.status(500).json({msg: "an error has occured", err});
+  });
 });
 
 module.exports = router;
